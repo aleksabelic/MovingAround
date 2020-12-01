4d6e27fc6c6241f22e81c35e0a195caeda89f2a8
aleksabelic <aleksa.belic@gmail.com>
2020-11-30 17:33:52 -0800

    initial

---
+++ Assets/SpawnCubes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCubes : MonoBehaviour
+{
+	public Transform prefab;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 1; i < 5; i++)
+        {
+            Instantiate(prefab, new Vector3(2.0F, 0, 0), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}

1beaf86af2f64719de1cfa01313d4c19004b2b72
aleksabelic <aleksa.belic@gmail.com>
2020-12-12 16:54:25 -0800

    zaLanu

---
+++ Assets/Restart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+
+	public SpawnCubes kocka;
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+       
+       	
+    }
+ 
+}
